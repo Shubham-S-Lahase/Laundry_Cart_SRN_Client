318395c5a359f0c49b4f6f85e86b9048bc68b94c
navinujuri <navinujuri@gmail.com>
2023-03-03 14:56:46 +0530

    component create order created

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
-  return <div className="App">
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage/>} />
-      </Routes>
-    </Router>
-  </div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
